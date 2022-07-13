fcdfd00ebe24b1c41d8ebf2261d2585b4c1e83c0
junee0530 <junee0530@naver.com>
2022-07-13 11:06:55 +0900

    2단계 완료

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,9 +3,13 @@ package com.ll.exam;
 import java.util.Scanner;
 
 public class App {
-    public void run() {
-        Scanner sc = new Scanner(System.in);
+    private Scanner sc; // private 한 이유 : 딱히 외부에 노출 될 필요없다.
+    // Scanner sc = new Scanner(System.in);    // 상관은 없지만 보통 스캐너는 생성자로 많이 한다.
+    App() {
+        sc = new Scanner(System.in);
+    }
 
+    public void run() {     // run은 외부(여기서는 Main) 에서 호출할 수 있어야 하기 때문에 private 하면 안된다.
         System.out.println("== 명언 SSG ==");
 
         outer:  // outer 라벨을 달면 이하에 있는 전체문이 break 된다.
@@ -14,11 +18,22 @@ public class App {
             String cmd = sc.nextLine();
 
             switch(cmd) {
+                case "등록":
+                    write();
+                    break ;
+
                 case "종료":
                     break outer;    // break만 하면 "종료" 라는 출력문 만 꺼진다 그러므로 outer 라벨을 달아준다.
             }
         }
 
         sc.close();
+    }
+
+    private void write() {
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
     }
 }
